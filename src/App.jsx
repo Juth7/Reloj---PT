7b6208b48f5883c2b89cfd995acd667c538b7f16
Juth7 <mjudith079@gmail.com>
2024-02-02 19:17:20 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import "./App.css";
+import Reloj from "./components/Reloj";
+
+class App extends Component {
+  render() {
+    return <Reloj />;
+  }
+}
+
+export default App;
---
+++ src/components/Reloj.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Reloj.css";
+
+class Reloj extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleTimeString("en-US"),
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleTimeString("en-US"),
+      });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Hora actual</h1>
+          <div className="card">
+            <h1>{this.state.time}</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Reloj;
